34173b1a80c975267784d73c0fdc4f371eaea17a
ndanbaev <akanbaevnurislam2002@gmail.com.com>
2025-08-29 12:26:55 +0500

    updateColor

--- Register/script.js
+++ Register/script.js
@@ -3,9 +3,9 @@ const signUpBtn = document.getElementById('signUp');
 const signInBtn = document.getElementById('signIn');
 
 signUpBtn.addEventListener('click', () => {
-  container.classList.add("right-panel-active");
+  container.classList.add('right-panel-active');
 });
 
 signInBtn.addEventListener('click', () => {
-  container.classList.remove("right-panel-active");
+  container.classList.remove('right-panel-active');
 });
